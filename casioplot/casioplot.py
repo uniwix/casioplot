523cbb0222a8a97fd28c6992bada5c6258ccaea3
Miguel Torrinha <miguel.torrinha.pereira@gmail.com>
2024-03-25 22:48:28 +0000

    made casio_settings more expandable
    I also renamed _clear_screen to _redraw_screen
    Created the configs.py file
    Fixed a bug in set_pixel, where some coordenates where being wrongfully considerade off bounds.

--- casioplot/casioplot.py
+++ casioplot/casioplot.py
@@ -7,119 +7,115 @@ Available functions:
   - :py:func:`clear_screen`
   - :py:func:`show_screen`
 
-You can also use :py:data:`casioplot_settings` to change some behavior.
+You can also use :py:data:`settings` to change some behavior.
 """
 
-import os
-
+from os import path
 from typing import Literal
-
 from PIL import Image, ImageDraw
+from configs import get_config
+
 
 _WHITE: tuple[int, int, int] = (255, 255, 255)  # RGBA white
+_BLACK: tuple[int, int, int] = (0, 0, 0)  # RGBA black
 
 # Create virtual screen
-_image: Image.Image = Image.new('RGB', (384, 192), _WHITE)
+_image: Image.Image = Image.new("RGB", (384, 192), _WHITE)
 # Create drawing object (used by the function draw_string
 _draw: ImageDraw.ImageDraw = ImageDraw.Draw(_image)
 
 
 class casioplot_settings:
     """Manage settings for the casioplot module."""
-    
-    class Settings:
-        """Store default settings"""
+
+    def __init__(self) -> None:
+        # all settings are the default ones
         # Size settings
-        width: int = 384  # Screen width in pixels
-        height: int = 192  # Screen height in pixels
-        
-        left_margin: int = 0
-        right_margin: int = 0
-        top_margin: int = 0
-        bottom_margin: int = 0
-        
+        self.width: int = 384  # Screen width in pixels
+        self.height: int = 192  # Screen height in pixels
+        # margins
+        self.left_margin: int = 0
+        self.right_margin: int = 0
+        self.top_margin: int = 0
+        self.bottom_margin: int = 0
+        # background Image
+        self.background_image: Image.Image = Image.new("RGB", (384, 192), _WHITE)
         # Output settings
-        open_image: bool = False  # Open the screen
-        save_image: bool = True  # Save the screen as an image
+        self.open_image: bool = False  # Open the screen
+        self.save_image: bool = True  # Save the screen as an image
         # Saving settings
-        filename: str = "casioplot.png"
-        image_format: str = "png"
-    
-    @classmethod
-    def _clear_screen(cls):
-        """Clear the screen.
-        
-        It creates a new image and assigns it to global _image.
-        """
-        global _image, _draw
-        
-        _image = Image.new('RGB', (
-            cls.left_margin + cls.width + cls.right_margin,
-            cls.top_margin + cls.height + cls.bottom_margin
-        ), _WHITE)  # Create a new white image
-        
-        _draw = ImageDraw.Draw(_image)
-    
-    @classmethod
-    def default(cls):
-        """Restore default parameters from the class to the instance."""
-        for k, v in casioplot_settings.Settings.__dict__.items():
-            setattr(cls, k, v)
-        cls._clear_screen()
-    
-    @classmethod
-    def casio_graph_90_plus_e(cls):
-        """Set the screen in the casio Graph 90+e format."""
-        global _image, _draw
-        cls.width = 384
-        cls.height = 192
-        cls.left_margin = 0
-        cls.right_margin = 0
-        cls.top_margin = 24
-        cls.bottom_margin = 0
-        
-        # Get the path of the template image
-        import os
+        self.filename: str = 'casioplot.png'
+        self.image_format: str = 'png'
 
-        file_path = os.path.join(os.path.abspath(os.path.dirname(__file__)),
-                                 'images',
-                                 'CASIO_Graph_90+e_empty.png')
-        _image = Image.open(file_path).convert("RGB")
-        _draw = ImageDraw.Draw(_image)
-    
-    @classmethod
-    def set(cls, **settings):
-        """Set an attribute for each given setting with the corresponding value.
-        """
+
+    def config_to(self, config: str = "default") -> None:
+        global _image
+        for setting, value in get_config(config).items():
+            setattr(self, setting, value)
+        _image = self.background_image
+
+
+    def set(self, **settings) -> None:
+        """Set an attribute for each given setting with the corresponding value."""
         for setting, value in settings.items():
-            setattr(cls, setting, value)
-        cls._clear_screen()
-    
-    @classmethod
-    def get(cls, setting):
-        return cls.__dict__.get(setting, cls.Settings.__dict__.get(setting))
+            setattr(self, setting, value)
+        _redraw_screen()
 
 
-def show_screen():
-    """Show the virtual screen
-    
-    This function implement two modes that can be enabled or disabled using the :py:class:`casioplot_settings`:
-      - Open the screen as an image (enabled using `casioplot_settings.get('open_image')`).
-      - Save the screen to the disk (enabled using `casioplot_settings.get('save_image')`).
-        The image is saved with the filename found in `casioplot_settings.get('filename')`
+    def get(self, setting: str):
+        """Returns an attribute"""
+        return getattr(self, setting)
+
+
+settings = casioplot_settings()
+
+
+def _redraw_screen() -> None:
+    """Redraws _image.
+
+    Only called when settings.set() is called,
+    used to redraw _image with custom margins, width and height.
     """
-    if casioplot_settings.get('open_image'):
+    global _image, _draw
+
+    # Create a new white image
+    _image = Image.new(
+        "RGB",
+        (
+            settings.left_margin + settings.width + settings.right_margin,
+            settings.top_margin + settings.height + settings.bottom_margin
+        ),
+        _WHITE
+    )
+
+    settings.background_image = _image
+
+    _draw = ImageDraw.Draw(_image)
+
+
+def show_screen() -> None:
+    """Show or saves the virtual screen
+
+    This function implement two modes that can be enabled or disabled using the :py:class:`settings`:
+      - Open the screen as an image (enabled using `settings.get('open_image')`).
+      - Save the screen to the disk (enabled using `settings.get('save_image')`).
+        The image is saved with the filename found in `settings.get('filename')`
+    """
+    if settings.get("open_image") is True:
         # open the picture
         _image.show()
-    if casioplot_settings.get('save_image'):
+    if settings.get("save_image") is True:
         # Save the screen to the disk as an image with the given filename
-        _image.save(casioplot_settings.get('filename'), format=casioplot_settings.get('image_format'))
+        _image.save(
+            settings.get("filename"),
+            format=settings.get("image_format"),
+        )
 
 
-def clear_screen():
+def clear_screen() -> None:
     """Clear the virtual screen."""
     show_screen()
-    casioplot_settings.default()
+    settings.config_to('default')
 
 
 def get_pixel(x: int, y: int) -> tuple[int, int, int] | None:
@@ -129,59 +125,73 @@ def get_pixel(x: int, y: int) -> tuple[int, int, int] | None:
     :param y: y coordinate (from the top)
     :return: The pixel color. A tuple that contain 3 integers from 0 to 255 or None if the pixel is out of the screen.
     """
-    if not 0 <= x <= casioplot_settings.get('width') - 1 or not 0 <= y <= casioplot_settings.get('height') - 1:
+    if (
+        not 0 <= x < settings.get("width")
+        or not 0 <= y < settings.get("height")
+    ):
         return None
     r: int
     g: int
     b: int
-    r, g, b = _image.getpixel((x + casioplot_settings.get('left_margin'), y + casioplot_settings.get('top_margin')))
+    r, g, b = _image.getpixel(
+        (
+            x + settings.get("left_margin"),
+            y + settings.get("top_margin"),
+        )
+    )
     return r, g, b
 
 
-def set_pixel(x: int, y: int, color: tuple[int, int, int] = (0, 0, 0)):
+def set_pixel(x: int, y: int, color: tuple[int, int, int] = (0, 0, 0)) -> None:
     """Set the RGB color of the pixel at the given position (from top left)
 
     :param x: x coordinate (from the left)
     :param y: y coordinate (from the top)
     :param color: The pixel color. A tuple that contain 3 integers from 0 to 255.
     """
-    if not 0 <= x < casioplot_settings.get('width') - 1 or not 0 <= y < casioplot_settings.get('height') - 1:
+    if (
+        not 0 <= x < settings.get("width")
+        or not 0 <= y < settings.get("height")
+    ):
         return
-    _image.putpixel((x + casioplot_settings.get('left_margin'), y + casioplot_settings.get('top_margin')), color)
+    _image.putpixel(
+        (
+            x + settings.get("left_margin"),
+            y + settings.get("top_margin"),
+        ),
+        color,
+    )
 
 
 def _get_filename(character, size: Literal["small", "medium", "large"] = "medium"):
     """Get the file where a character is saved and return the ``space`` file if the character doesn't exist.
-    
+
     :param character: The character to find
     :param size: The size of the character
     :return: The character filename. A string: "{``./chars`` folder absolute path}/{character}_{size}.png"
     """
-    special_chars = {
-        ' ': 'space'
-    }
-    filename = special_chars.get(character, character) + '.txt'
-    file_path = os.path.join(os.path.abspath(os.path.dirname(__file__)),
-                             'chars',
-                             size,
-                             filename)
+    special_chars = {" ": "space"}
+    filename = special_chars.get(character, character) + ".txt"
+    file_path = path.join(
+        path.abspath(path.dirname(__file__)), "chars", size, filename
+    )
 
-    if not os.path.isfile(file_path):
+    if not path.isfile(file_path):
         print(f'WARNING: No character "{character}" found for size "{size}".')
-        file_path = os.path.join(os.path.abspath(os.path.dirname(__file__)),
-                                 'chars',
-                                 size,
-                                 'space.txt')
+        file_path = path.join(
+            path.abspath(path.dirname(__file__)), "chars", size, "space.txt"
+        )
     return file_path
 
 
-def draw_string(x: int,
-                y: int,
-                text: str,
-                color: tuple[int, int, int] = (0, 0, 0),
-                size: Literal["small", "medium", "large"] = "medium"):
+def draw_string(
+    x: int,
+    y: int,
+    text: str,
+    color: tuple[int, int, int] = (0, 0, 0),
+    size: Literal["small", "medium", "large"] = "medium") -> None:
     """Draw a string on the virtual screen with the given RGB color and size.
-    
+
     :param x: x coordinate (from the left)
     :param y: y coordinate (from the top)
     :param text: text that will be shown
@@ -189,24 +199,22 @@ def draw_string(x: int,
     :param size: Size of the text. String from the following values: "small", "medium" or "large".
     :raise ValueError: Raise a ValueError if the size isn't correct.
     """
-    sizes = {
-        'small': (10, 10),
-        'medium': (13, 17),
-        'large': (18, 23)
-    }
-    
+    sizes = {"small": (10, 10), "medium": (13, 17), "large": (18, 23)}
+
     if size not in sizes.keys():
-        raise ValueError(f'Unknown size "{size}". Size must be one of the following: "small", "medium" or "large"')
-    
+        raise ValueError(
+            f'Unknown size "{size}". Size must be one of the following: "small", "medium" or "large"'
+        )
+
     for character in text:
         filename = _get_filename(character, size)
         n = 0
-        with open(filename, 'r') as c:
+        with open(filename, "r") as c:
             count = 0
             for line in c:
                 for j, k in enumerate(line):
                     if k in ["$"]:
-                        set_pixel(x+j, y+count, color)
+                        set_pixel(x + j, y + count, color)
                     n = j
                 count += 1
         x += n
---
+++ casioplot/configs.py
@@ -0,0 +1,45 @@
+from PIL import Image
+from os import path
+
+_WHITE: tuple[int, int, int] = (255, 255, 255)  # RGBA white
+_images_directory = path.join(
+    path.abspath(path.dirname(__file__)),
+    "images"
+)
+
+configs = {
+    'default': {
+        'width': 384,
+        'height': 192,
+        'left_margin': 0,
+        'right_margin': 0,
+        'top_margin': 0,
+        'bottom_margin': 0,
+        'background_image': Image.new('RGB', (384, 192), _WHITE),
+        'open_image': False,
+        'save_image': True,
+        'filename': 'casioplot.png',
+        'image_format': 'png'
+    },
+    'casio_graph_90_plus_e': {
+        'width': 384,
+        'height': 192,
+        'left_margin': 0,
+        'right_margin': 0,
+        'top_margin': 24,
+        'bottom_margin': 0,
+        'background_image': Image.open(
+            path.join(_images_directory, "CASIO_Graph_90+e_empty.png")
+        ).convert('RGB'),
+        'open_image': False,
+        'save_image': True,
+        'filename': 'casioplot.png',
+        'image_format': 'png'
+    }
+}
+
+
+def get_config(config: str) -> dict:
+    if config not in configs.keys():
+        raise ValueError(f"No config called {config}")
+    return configs[config]
