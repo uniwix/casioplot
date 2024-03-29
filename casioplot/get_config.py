f997c299cc08e4afacc37b620430bb538ad242c4
Miguel Torrinha <miguel.torrinha.pereira@gmail.com>
2024-03-29 16:56:08 +0000

    Improved _toml_to_configuration

--- casioplot/get_config.py
+++ casioplot/get_config.py
@@ -61,6 +61,34 @@ def _set_settings(toml: dict) -> configuration:
     return _toml_to_configuration(toml, config)
 
 
+_toml_settings = {
+    "canvas": (
+        "width",
+        "height"
+    ),
+    "margins": (
+        "left_margin",
+        "right_margin",
+        "top_margin",
+        "bottom_margin"
+    ),
+    "background": (
+        "bg_image_is_set",
+        "background_image"
+    ),
+    "show_screen": (
+        "show_screen"
+    ),
+    "saving_screen": (
+        "save_screen",
+        "filename",
+        "image_format",
+        "save_multiple",
+        "save_rate"
+    )
+}
+
+
 def _toml_to_configuration(toml: dict, config: configuration) -> configuration:
     """Add the settings from a TOML dictionary to a configuration dictionary.
 
@@ -69,39 +97,10 @@ def _toml_to_configuration(toml: dict, config: configuration) -> configuration:
     :return: The configuration dictionary with the new settings.
     """
 
-    if "size" in toml:
-        if "width" in toml["size"]:
-            config["width"] = toml["size"]["width"]
-        if "height" in toml["size"]:
-            config["height"] = toml["size"]["height"]
-
-    if "margins" in toml:
-        if "left" in toml["margins"]:
-            config["left_margin"] = toml["margins"]["left"]
-        if "right" in toml["margins"]:
-            config["right_margin"] = toml["margins"]["right"]
-        if "top" in toml["margins"]:
-            config["top_margin"] = toml["margins"]["top"]
-        if "bottom" in toml["margins"]:
-            config["bottom_margin"] = toml["margins"]["bottom"]
-
-    if "background" in toml:
-        if "background" in toml["background"]:
-            config["background_image"] = toml["background"]["background_image"]
-        if "bg_image_is_set" in toml["background"]:
-            config["bg_image_is_set"] = toml["background"]["bg_image_is_set"]
-
-    if "screen" in toml:
-        if "show" in toml["screen"]:
-            config["show_screen"] = toml["screen"]["show"]
-        if "save" in toml["screen"]:
-            config["save_screen"] = toml["screen"]["save"]
-        if "rate" in toml["screen"]:
-            config["save_rate"] = toml["screen"]["rate"]
-            config["save_multiple"] = True
-        if "filename" in toml["screen"]:
-            config["filename"] = toml["screen"]["filename"]
-        if "format" in toml["screen"]:
-            config["image_format"] = toml["screen"]["format"]
+    for section, settings in _toml_settings.items():
+        if section in toml:
+            for setting in settings:
+                if setting in toml[section]:
+                    config[setting] = toml[section][setting]
 
     return config
