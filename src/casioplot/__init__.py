f645e27ca08ca0e44d0cfb063503ac41e0aee152
uniwix <uniwixu@gmail.com>
2024-04-03 23:53:36 +0200

    fix `__init__.py` not importing functions

--- src/casioplot/__init__.py
+++ src/casioplot/__init__.py
@@ -3,4 +3,6 @@
 All public functions from the :py:mod:`casioplot` module are accessible in this package.
 """
 
+from casioplot.casioplot import set_pixel, get_pixel, draw_string, show_screen, clear_screen
+
 __version__ = "3.1.3"
