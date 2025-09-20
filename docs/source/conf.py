a5a2b85864f73fef8051428851fae7e065cc1751
Uniwix <uniwixu@gmail.com>
2025-09-20 10:52:36 +0200

    Bump version: 3.4.1 → 3.5.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = '2021, uniwix'
 author = 'uniwix'
 
 # The full version, including alpha/beta/rc tags
-release = '3.4.1'
+release = '3.5.0'
 
 
 # -- General configuration ---------------------------------------------------
